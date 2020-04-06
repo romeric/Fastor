82411f621ba806e9ab7cf17b8ada87d85f9620ad
romeric <roman_poya@yahoo.com>
2020-04-06 06:54:09 +0100

    Two layer timeit function for runs and samples similar to ipython timeit

--- Fastor/commons/timeit.h
+++ Fastor/commons/timeit.h
@@ -1,11 +1,14 @@
 #ifndef TIMEIT_H
 #define TIMEIT_H
 
+#include <algorithm>
+#include <array>
 #include <chrono>
 #include <cmath>
 #include <cstdint>
 #include <iomanip>
 #include <iostream>
+#include <numeric>
 #include <string>
 #include <limits>
 #include <tuple>
@@ -77,6 +80,10 @@ namespace Fastor {
 #define FASTOR_SIMPLE_RDTSC
 #endif
 
+#ifndef FASTOR_RDTSC_OVERHEAD
+#define FASTOR_RDTSC_OVERHEAD 0UL
+#endif
+
 #ifndef FASTOR_BENCH_RUNTIME
 #define FASTOR_BENCH_RUNTIME 1.0
 #endif
@@ -85,6 +92,12 @@ namespace Fastor {
 // Get cpu cycle count
 #ifdef _WIN32
 inline uint64_t rdtsc() {
+    return __rdtsc();
+}
+inline uint64_t rdtsc_begin() {
+    return __rdtsc();
+}
+inline uint64_t rdtsc_end() {
     return __rdtsc();
 }
 //  Linux/GCC
@@ -175,7 +188,7 @@ std::string format_time_string(double _time) {
     << useless::format_time(mean_time) << useless::format_time_string(mean_time) << ". "\
     << FGRN(BOLD("max time: "))\
     << useless::format_time(worst_time) << useless::format_time_string(worst_time) << ". "\
-    << FGRN(BOLD("No of RDTSC CPU cycles "))\
+    << FGRN(BOLD("Average no of RDTSC CPU cycles "))\
     << uint64_t(cycles/(1.0*counter)) << std::endl;\
 
 #define FASTOR_FORMAT_BENCH_TIME_DISPLAY_2()\
@@ -197,6 +210,7 @@ std::tuple<double,double,double> // min, mean, max times
 timeit(T (*func)(Params...), Args...args)
 {
     uint64_t counter = 1;
+    double accum_time = 0.0;
     double mean_time = 0.0;
     double best_time = std::numeric_limits<double>::max();
     double worst_time = 0.0;
@@ -209,12 +223,123 @@ timeit(T (*func)(Params...), Args...args)
     // https://stackoverflow.com/questions/36663379/seconds-calculation-using-rdtsc
     double tsc_to_time = 1.0/cpuID.EBX();
 
-// #ifndef _WIN32
 //     rdtsc_begin();
 //     rdtsc_end();
-// #endif
 #endif
-            // std::cout << () * (cycles/(1.0*counter)) << "\n";
+
+    // We collect many samples
+    constexpr uint64_t num_samples            = 100UL;
+    // Every sample is composed of multiple runs
+    constexpr uint64_t num_run_per_sample     = 5E6;
+
+    auto __min_element = [&](std::array<double,num_samples> &a) {
+        double v = a[0];
+        for (uint64_t n = 1; n < a.size(); n++) {
+            if ((a[n] < v && (a[n] != 0 || a[n] > 1E-14) ) || v == 0) v = a[n];
+        }
+        return v;
+    };
+
+    std::array<double,num_samples> best_times;
+    std::fill(best_times.begin(),best_times.end(),std::numeric_limits<double>::max());
+    std::array<double,num_samples> mean_times = {};
+    std::array<double,num_samples> worst_times = {};
+
+    uint64_t num_collected_samples = 0;
+
+    for (uint64_t sample = 0; sample < num_samples && accum_time < FASTOR_BENCH_RUNTIME; ++sample) {
+
+        for (uint64_t run=0; run < num_run_per_sample; ++run)
+        {
+#if defined(FASTOR_USE_SYSTEM_CLOCK)
+            std::chrono::time_point<std::chrono::system_clock> start, end;
+            start = std::chrono::system_clock::now();
+#elif defined(FASTOR_USE_RDTSC)
+            auto cycle = rdtsc_begin();
+#else
+            std::chrono::time_point<std::chrono::steady_clock> start, end;
+            start = std::chrono::steady_clock::now();
+#endif
+            // Run the function
+            func(std::forward<Params>(args)...);
+            // Ignore the few first runs for cache hot measurements
+            if (run < 1 && sample < 1) continue;
+
+#if defined(FASTOR_USE_RDTSC)
+            cycle = rdtsc_end() - cycle - FASTOR_RDTSC_OVERHEAD;
+            cycles += cycle;
+            double elapsed_t = tsc_to_time*cycle;
+            if (elapsed_t < best_times[sample] && (elapsed_t != 0.0 || elapsed_t > 1E-15)) {
+                best_times[sample] = elapsed_t;
+            };
+            if (elapsed_t > worst_times[sample]) {
+                worst_times[sample] = elapsed_t;
+            };
+            mean_times[sample] += elapsed_t;
+            accum_time += elapsed_t;
+#else
+            end = std::chrono::steady_clock::now();
+            std::chrono::duration<double> elapsed_seconds = end-start;
+
+            if (elapsed_seconds.count() < best_times[sample] && elapsed_seconds.count() != 0.0) {
+                best_times[sample] = elapsed_seconds.count();
+            };
+            if (elapsed_seconds.count() > worst_times[sample]) {
+                worst_times[sample] = elapsed_seconds.count();
+            };
+            mean_times[sample] += elapsed_seconds.count();
+            accum_time += elapsed_seconds.count();
+#endif
+            counter++;
+
+            if (accum_time > FASTOR_BENCH_RUNTIME)
+            {
+                break;
+            }
+        }
+        num_collected_samples++;
+    }
+
+    best_time   = __min_element(best_times);
+    worst_time  = *std::max_element(worst_times.begin(),worst_times.begin()+num_collected_samples);
+    mean_time   = std::accumulate(mean_times.begin(), mean_times.begin()+num_collected_samples, 0.0);
+
+    mean_time /= (double)counter;
+#if defined(FASTOR_USE_RDTSC)
+    FASTOR_FORMAT_BENCH_TIME_DISPLAY_1()
+#else
+    FASTOR_FORMAT_BENCH_TIME_DISPLAY_2()
+#endif
+
+    return std::make_tuple(best_time, mean_time, worst_time);
+}
+
+
+
+
+#if 0
+// One loop timer
+template<typename T, typename ... Params, typename ... Args>
+inline
+std::tuple<double,double,double> // min, mean, max times
+timeit(T (*func)(Params...), Args...args)
+{
+    uint64_t counter = 1;
+    double mean_time = 0.0;
+    double best_time = std::numeric_limits<double>::max();
+    double worst_time = 0.0;
+#if defined(FASTOR_USE_RDTSC)
+    uint64_t cycles = 0;
+    CPUID cpuID(0);
+    // A cycle is 1 second per max cpu frequency assuming constant_tsc
+    // Caution: in theory, there is no guarantee that rdtsc would have
+    // strong relation to CPU cycles
+    // https://stackoverflow.com/questions/36663379/seconds-calculation-using-rdtsc
+    double tsc_to_time = 1.0/cpuID.EBX();
+
+//     rdtsc_begin();
+//     rdtsc_end();
+#endif
 
     for (auto iter=0; iter<1e09; ++iter)
     {
@@ -222,11 +347,7 @@ timeit(T (*func)(Params...), Args...args)
         std::chrono::time_point<std::chrono::system_clock> start, end;
         start = std::chrono::system_clock::now();
 #elif defined(FASTOR_USE_RDTSC)
-#ifdef _WIN32
-        auto cycle = rdtsc();
-#else
         auto cycle = rdtsc_begin();
-#endif
 #else
         std::chrono::time_point<std::chrono::steady_clock> start, end;
         start = std::chrono::steady_clock::now();
@@ -238,11 +359,7 @@ timeit(T (*func)(Params...), Args...args)
         if (iter < 1) continue;
 
 #if defined(FASTOR_USE_RDTSC)
-#ifdef _WIN32
-        cycle = rdtsc() - cycle;
-#else
         cycle = rdtsc_end() - cycle;
-#endif
         cycles += cycle;
 
         double elapsed_t = tsc_to_time*cycle;
@@ -265,7 +382,6 @@ timeit(T (*func)(Params...), Args...args)
         };
         mean_time += elapsed_seconds.count();
 #endif
-
         counter++;
 
         if (mean_time > FASTOR_BENCH_RUNTIME)
@@ -280,8 +396,9 @@ timeit(T (*func)(Params...), Args...args)
         }
     }
 
-    return std::make_tuple(mean_time, best_time, worst_time);
+    return std::make_tuple(best_time, mean_time, worst_time);
 }
+#endif
 
 
 
