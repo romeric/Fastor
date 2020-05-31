0f612b3c2fc2cd73b46e8b12c8f1ae44e09065b8
romeric <roman_poya@yahoo.com>
2020-05-31 02:15:23 +0100

    Shut MSVC warnings and disable explicit single einsum tests for building on appveyor

--- Fastor/Fastor.h
+++ Fastor/Fastor.h
@@ -1,6 +1,11 @@
 #ifndef FASTOR_ALL_INCLUDE_H
 #define FASTOR_ALL_INCLUDE_H
 
+// Disable MSVC warnings for macros
+#pragma warning (disable: 4003)
+// Disable MSVC warnings for conversion
+#pragma warning (disable: 4244)
+
 #include "commons/utils.h"
 #include "simd_vector/SIMDVector.h"
 #include "simd_math/simd_math.h"
@@ -15,6 +20,9 @@
 #include "tensor_algebra/abstract_contraction.h"
 #include "expressions/expressions.h"
 #include "backend/voigt.h"
+
+#pragma warning (default: 4003)
+#pragma warning (default: 4244)
 
 #endif // FASTOR_ALL_INCLUDE_H
 
