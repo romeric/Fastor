085860f3017a7aee9b4a8e7013d6b0af25e1999f
Fabien Péan <fabien@pean.pro>
2021-10-13 17:53:33 +0200

    Make assignment operator signature like built-in types

--- Fastor/tensor/TensorInplaceOperators.h
+++ Fastor/tensor/TensorInplaceOperators.h
@@ -4,46 +4,54 @@
 // CRTP Overloads for nth rank Tensors
 //---------------------------------------------------------------------------------------------//
 template<typename Derived, size_t DIMS>
-FASTOR_INLINE void operator +=(const AbstractTensor<Derived,DIMS>& src_) {
+FASTOR_INLINE auto& operator +=(const AbstractTensor<Derived,DIMS>& src_) {
     assign_add(*this, src_.self());
+    return *this;
 }
 
 template<typename Derived, size_t DIMS>
-FASTOR_INLINE void operator -=(const AbstractTensor<Derived,DIMS>& src_) {
+FASTOR_INLINE auto& operator -=(const AbstractTensor<Derived,DIMS>& src_) {
     assign_sub(*this, src_.self());
+    return *this;
 }
 
 template<typename Derived, size_t DIMS>
-FASTOR_INLINE void operator *=(const AbstractTensor<Derived,DIMS>& src_) {
+FASTOR_INLINE auto& operator *=(const AbstractTensor<Derived,DIMS>& src_) {
     assign_mul(*this, src_.self());
+    return *this;
 }
 
 template<typename Derived, size_t DIMS>
-FASTOR_INLINE void operator /=(const AbstractTensor<Derived,DIMS>& src_) {
+FASTOR_INLINE auto& operator /=(const AbstractTensor<Derived,DIMS>& src_) {
     assign_div(*this, src_.self());
+    return *this;
 }
 //---------------------------------------------------------------------------------------------//
 
 // Scalar overloads for in-place operators
 //---------------------------------------------------------------------------------------------//
 template<typename U=T, enable_if_t_<is_arithmetic_v_<U>,bool> = 0 >
-FASTOR_INLINE void operator +=(U num) {
+FASTOR_INLINE auto& operator +=(U num) {
     trivial_assign_add(*this, num);
+    return *this;
 }
 
 template<typename U=T, enable_if_t_<is_arithmetic_v_<U>,bool> = 0 >
-FASTOR_INLINE void operator -=(U num) {
+FASTOR_INLINE auto& operator -=(U num) {
     trivial_assign_sub(*this, num);
+    return *this;
 }
 
 template<typename U=T, enable_if_t_<is_arithmetic_v_<U>,bool> = 0 >
-FASTOR_INLINE void operator *=(U num) {
+FASTOR_INLINE auto& operator *=(U num) {
     trivial_assign_mul(*this, num);
+    return *this;
 }
 
 template<typename U=T, enable_if_t_<is_arithmetic_v_<U>,bool> = 0 >
-FASTOR_INLINE void operator /=(U num) {
+FASTOR_INLINE auto& operator /=(U num) {
     trivial_assign_div(*this, num);
+    return *this;
 }
 //---------------------------------------------------------------------------------------------//
 
